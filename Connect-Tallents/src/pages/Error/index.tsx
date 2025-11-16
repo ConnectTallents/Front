438bfda384588fa67f7550345a46d1fa494f1361
Jounaxis <jgs123098@gmail.com>
2025-11-16 19:39:12 -0300

    feat: Estruturação da rota de erro e estilização da mesma

--- Connect-Tallents/src/pages/Error/index.tsx
+++ Connect-Tallents/src/pages/Error/index.tsx
@@ -1,7 +1,24 @@
-export default function Error () {
-    return(
-        <main>
-            <h1> Página de erro </h1>
+import { Link } from "react-router-dom";
+import Background from "../../components/Background/Background";
+
+export default function Error() {
+    return (
+        <main className="erro-container">
+
+            <Background />
+
+            <div className="erro-card">
+                <h1 className="erro-titulo">Página Não Encontrada</h1>
+                <p className="erro-texto">
+                    Parece que você tentou acessar uma rota que não existe.
+                    Verifique o endereço ou volte para a página inicial.
+                </p>
+
+                <Link to="/" className="erro-botao">
+                    Voltar para Home
+                </Link>
+            </div>
+
         </main>
-    )
+    );
 }

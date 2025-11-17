bea3819dc8cf845b4e9a7f17e58bea5eb452b95a
Lucas06-ux <lubgian@gmail.com>
2025-11-16 21:42:11 -0300

    feat: pagina de contato, com estilização além da criação de componentes proprios para a página

--- Connect-Tallents/src/routes/AppRoutes.tsx
+++ Connect-Tallents/src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Error from "../pages/Error";
 import Integrantes from "../pages/Integrantes";
 import Sobre from "../pages/Sobre";
+import Contato from "../pages/Contato";
 
 export const routes = createBrowserRouter([
     {
@@ -24,6 +25,11 @@ export const routes = createBrowserRouter([
             {
                 path: "/sobre",
                 element: <Sobre />
+            }
+            ,
+            {
+                path: "/contato",
+                element: <Contato />
             }
         ]
     }

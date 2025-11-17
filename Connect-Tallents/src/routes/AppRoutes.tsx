1e9819998729a745cc0171ff15878856eefaf64c
Lucas06-ux <lubgian@gmail.com>
2025-11-16 21:32:08 -0300

    feat: pagina sobre montada com imagens e descrições

--- Connect-Tallents/src/routes/AppRoutes.tsx
+++ Connect-Tallents/src/routes/AppRoutes.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
 import Integrantes from "../pages/Integrantes";
+import Sobre from "../pages/Sobre";
 
 export const routes = createBrowserRouter([
     {
@@ -13,10 +14,16 @@ export const routes = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />
-            },
+            }
+            ,
             {
                 path: "/integrantes",
                 element: <Integrantes />
+            }
+            ,
+            {
+                path: "/sobre",
+                element: <Sobre />
             }
         ]
     }
